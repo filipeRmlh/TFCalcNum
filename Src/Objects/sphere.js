b62c5c0401455daf7577268eb3b9b9100b5def08
filipe <filiperamalho@bitbucket.com>
2017-10-27 16:54:42 -0200

    reformulando o movimento das esferas

--- Src/Objects/sphere.js
+++ Src/Objects/sphere.js
@@ -12,7 +12,7 @@ var Sphere = function(position,options){
       this.massa=3/4*Math.PI*Math.pow(this.r,3)*this.material.density;
     }
     this.element= new Circle(new Vector(position.x,position.y),this.r,options);
-    this.kfluid = -1/2*ENV.density*0.47*Math.PI*Math.pow(this.r*1,2)*100;
+    this.kfluid = -1/2*ENV.density*0.47*Math.PI*Math.pow(this.r*1,2)/1000;
     this.posk0 = position;
     this.posk1 = position;
     this.moveLoop;
@@ -26,8 +26,10 @@ Sphere.prototype={
   movement:function(){
     var h = 1/this.h;
     var posk2={y:0,x:0};
-      posk2.y = (2*Math.pow(h,2)*this.massa*ENV.g-(2*this.massa+this.kfluid*h)*this.posk0.y+4*this.massa*this.posk1.y)/(2*this.massa-this.kfluid*h);
-      posk2.x = (-(2*this.massa+this.kfluid*h)*this.posk0.x+4*this.massa*this.posk1.x)/(2*this.massa-this.kfluid*h);
+      // posk2.y = (2*Math.pow(h,2)*this.massa*ENV.g-(2*this.massa+this.kfluid*h)*this.posk0.y+4*this.massa*this.posk1.y)/(2*this.massa-this.kfluid*h);
+      posk2.y = (2*Math.pow(h,2)*this.massa*ENV.g-(2*this.massa-this.kfluid*h)*this.posk0.y+4*this.massa*this.posk1.y)/(2*this.massa+this.kfluid*h);
+      // posk2.x = (-(2*this.massa+this.kfluid*h)*this.posk0.x+4*this.massa*this.posk1.x)/(2*this.massa-this.kfluid*h);
+      posk2.x = (-(2*this.massa-this.kfluid*h)*this.posk0.x+4*this.massa*this.posk1.x)/(2*this.massa+this.kfluid*h);
       this.posk0 = this.posk1;
       this.posk1 = {x:posk2.x,y:posk2.y};
       this.setPosition(posk2.x,posk2.y);
@@ -49,8 +51,7 @@ Sphere.prototype={
       var theta = Normative.getTheta(objFormat.start, objFormat.end);
       var h = this.h;
       var v = Normative.multiplyToEscalar(Normative.subtractVectors(this.posk1,this.posk0),1/h);
-      var modv = Normative.module(v);
-    //  modv = modv>3?modv-0.7:(modv<2?0:modv);
+      var modv = Normative.module(v)*0.983;
       var refV = Normative.reflectVector(Normative.normalize(v),theta);
       refV = Normative.multiplyToEscalar(refV,modv);
       this.posk1.x = this.posk0.x+(refV.x)*(h);
